c8f34698031e1c0099c8da2972d1904253e117e5
aliekberkara <79039580+aliekberkara@users.noreply.github.com>
2021-11-24 03:48:12 +0300

    Create Type_Casting.java

---
+++ Basic_Exercises/Type_Casting.java
@@ -0,0 +1,23 @@
+package Ornekler;
+
+public class Type_Casting {
+	public static void main(String[] args) {
+		
+		String a = "4";
+		String b = "6";
+		String c = "9";
+		String d = "8";
+		String e = "5";
+		
+		int a1 = Integer.parseInt(a);
+		int b1 = Integer.parseInt(b);
+		int c1 = Integer.parseInt(c);
+		int d1 = Integer.parseInt(d);
+		int e1 = Integer.parseInt(e);
+		
+		System.out.println(a1+b1+c1+d1+e1);
+		System.out.println(a1*b1*c1*d1*e1);
+		System.out.println(a1-b1);
+		System.out.println(a1/b1);
+	}
+}
