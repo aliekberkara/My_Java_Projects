ee82c520213dac318015492e8520ee05e75fd53f
aliekberkara <79039580+aliekberkara@users.noreply.github.com>
2021-11-24 12:30:19 +0300

    Update Anagram.java

--- Basic_Exercises/Anagram.java
+++ Basic_Exercises/Anagram.java
@@ -1,9 +1,9 @@
 /*
-Enter two words from the keyboard. Write the codes of the application that checks whether these words are Anagrams. 
+Enter two words from the keyboard. Write the codes of the application that checks whether these words are Anagrams.
 (An anagram is when letters in a word or phrase are swapped to form another word. For example, "brief" and "fiber" are anagrams.)
 */
 
-package Ornekler;
+package Basic_Exercises;
 import java.util.*;
 
 public class Anagram {
@@ -13,16 +13,16 @@ public class Anagram {
 		try (Scanner scan = new Scanner(System.in)) {
 			String word1 = scan.nextLine();
 			String word2 = scan.nextLine();
-			
+
 			char[] sequence1 = new char[word1.length()];
 			char[] sequence2 = new char[word2.length()];
-			
+
 			sequence1 = word1.toCharArray();
 			sequence2 = word2.toCharArray();
-			
+
 			Arrays.sort(sequence1);
 			Arrays.sort(sequence2);
-			
+
 			if(Arrays.equals(sequence1, sequence2) == true) {
 				System.out.println("The words " + word1 + " and " + word2 + " are Anagram words.");
 			}
@@ -30,7 +30,7 @@ public class Anagram {
 				System.out.println("The words " + word1 + " and " + word2 + " are not Anagram words.");
 			}
 		}
-		
+
 	}
 
 }
